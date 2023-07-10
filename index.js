d12b0b0c0a18dce396392dad2f954a58938c5e39
Dusty Brigsby <108590519+dustybrigsby@users.noreply.github.com>
2023-07-10 13:01:30 -0400

    Updated index.js
    Updated to align with challenge requirements to first meet minimum viable product.

--- index.js
+++ index.js
@@ -8,8 +8,13 @@ const Color = require('./lib/colors.js');
 const questions = [
     {
         type: 'input',
-        name: 'size',
-        message: 'What size for your logo? (square, in pixels up to 500px. default: 100px)',
+        name: 'text',
+        message: 'Type 1 to 3 letters to superimpose over your shape:'
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'What color for this text? (hex value or color name - full list found at https://www.w3schools.com/colors/colors_names.asp)'
     },
     {
         type: 'list',
@@ -21,54 +26,38 @@ const questions = [
         type: 'input',
         name: 'shapeColor',
         message: 'What color for this shape? (hex value or color name - full list found at https://www.w3schools.com/colors/colors_names.asp)'
-    },
-    {
-        type: 'input',
-        name: 'text',
-        message: 'Type 1 to 3 letters to superimpose over your shape:'
-    },
-    {
-        type: 'input',
-        name: 'textColor',
-        message: 'What color for this text? (hex value or color name - full list found at https://www.w3schools.com/colors/colors_names.asp)'
     }
 ];
 
 async function getInput() {
     try {
         const answers = await inquirer.prompt(questions);
+        console.log('answers:', answers);
 
         const data = {
-            size: answers.size,
-            shape: answers.shape,
-            shapeColor: answers.shapeColor,
             text: answers.text,
-            textColor: answers.textColor
+            textColor: answers.textColor,
+            shape: answers.shape,
+            shapeColor: answers.shapeColor
         };
 
-        if (!data.size) {
-            data.size = 100;
-        }
-        console.log('data:', data);
 
-        const shapeString = new Shape(data.shape, data.shapeColor).makeShape();
 
-        const fileName = 'logo.svg';
-
-        writeToFile(fileName, shapeString);
+        writeToFile(svgData);
 
     } catch (error) {
-        console.error('Error:', error);
+        console.error('getInput Error:', error);
     }
 }
 
-async function writeToFile(fileName, data, increment = 0) {
-    const name = `${ path.basename(filename, path.extname(filename)) }${ increment || "" }${ path.extname(filename) }`;
-    return await fs.writeFile(name, data, { encoding: 'utf8', flag: 'wx' })
-        .catch(async ex => {
-            if (ex.code === "EEXIST") return await writeFile(filename, data, increment += 1);
-            throw ex;
-        }) || name;
+function writeToFile(data) {
+    fs.writeFile('examples/logo.svg', data, (err) => {
+        if (err) {
+            console.log('Error while writing file:', err);
+        } else {
+            console.log('Generated logo.svg');
+        }
+    });
 }
 
 function init() {
